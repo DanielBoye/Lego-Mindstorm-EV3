02819fa1a2396666da2a57ac406c8bfd45d82ca1
DanielBoye <83395536+DanielBoye@users.noreply.github.com>
2022-12-19 09:29:34 +0100

    Update main.py

--- server/main.py
+++ server/main.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env pybricks-micropython
+# Før du kjører dette programmet, sørg for at klient- og server EV3-klossene er
+# paret med Bluetooth, men IKKE koble dem til. Programmet vil ta vare
+# for å etablere forbindelsen.
 
-# Before running this program, make sure the client and server EV3 bricks are
-# paired using Bluetooth, but do NOT connect them. The program will take care
-# of establishing the connection.
-
-# The server must be started before the client!
+# Serveren må startes opp før klienten!
 
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import TouchSensor
@@ -13,43 +11,54 @@ from pybricks.tools import wait
 from pybricks.messaging import BluetoothMailboxServer, TextMailbox
 import time
 
-# Initialize server
+# Initialisere serveren
 server = BluetoothMailboxServer()
+
+# Sette meldingsboks som en string til variabelen mbox
 mbox = TextMailbox('greeting', server)
 
-# Initialize the EV3 Brick
+# Initialisere
 ev3 = EV3Brick()
 
-# Set volume to 100% and make a beep to signify program has started
-
-
-# Initialize EV3 touch sensor and motors
-
+# En funksjon for soving siden jeg kan endre tiden imellom hvert signal samlet inni her 
 def sov():
     time.sleep(0.005)
 
-# The server must be started before the client!
+
 print('Waiting for connection...')
+
+# Venter på et signal
 server.wait_for_connection()
 print('Connected!')
+# Når den har koblet seg til vil jo koden kjøre videre og dette vet vi med at roboten piper 
 ev3.speaker.set_volume(100)
 ev3.speaker.beep()
 
 # Create a loop to react to buttons
 while True:
-   
     
-    # FERDIG
-
+    # Forklarer denne delen bare en gang, dette gjelder for mesteparten av koden
+    
+    # Hvis knappen opp er trykket
     if Button.UP in ev3.buttons.pressed():
-        # Console output
+        # Print Bak i terminalen 
         print("Bak")
+        # Denne sove funksjonen er her for å forhindre en buffer overflow i koden
+        # Roboten tåler ikke å bli sendt 500 meldinger i sekundet og den får for mye informasjon på 
+        # en gang, og den bare slutter til slutt
+        # Derfor har jeg denne her at den sender ut oppdateringer av status i intervaller
         sov()
+        # Sende signalet 
         mbox.send('D = 1')
     else:
+        # Hvis ikke det så må den sove litt og sende signalet D = 0
         sov()
         mbox.send('D = 0')
     
+    # D er egentlig DRIVE men når den kjører bakover går motorene raskerer (jeg vet ikke hvorfor)
+    # Derfor bare flippet jeg dette om som roboten kjører raskere frem
+    
+    # Det samme som den øvre delen bare motsatt
     if Button.DOWN in ev3.buttons.pressed():
         # Console output
         print("Fram")
@@ -59,8 +68,9 @@ while True:
         time.sleep(0.05)
         mbox.send('F = 0')
     
+    # Sjekker om både knappen opp OG om knappet til høyre er trykket
     if Button.UP in ev3.buttons.pressed() and Button.RIGHT in ev3.buttons.pressed():
-        # Console output
+        # Se over for forklaring
         print("Fram Høyre")
         sov()
         mbox.send('FH = 1')
@@ -95,13 +105,6 @@ while True:
         sov()
         mbox.send('L = 0')
     
-    
-
-
-
-
-
-
     if Button.CENTER in ev3.buttons.pressed():
         # Console output
         print("Midten")
@@ -110,15 +113,3 @@ while True:
     else:
         sov()
         mbox.send('M = 0')
-
-    
-
-
-# Send message to client to stop
-mbox.send('FERDIG')
-
-# Use the speech tool to signify the program has finished
-# ev3.speaker.say("Program complete")
-
-# Write your program here.
-ev3.speaker.beep()
